aad2433b435e1c1fc23cd728d6e0e6d6765620cf
Brayden Chan <72172597+megabyte6@users.noreply.github.com>
2022-12-06 23:16:49 -0800

    Add static methods to open new fxml views.

--- src/main/java/com/megabyte6/wordle/App.java
+++ src/main/java/com/megabyte6/wordle/App.java
@@ -1,8 +1,11 @@
 package com.megabyte6.wordle;
 
+import java.io.IOException;
+
+import com.megabyte6.wordle.util.Data;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -16,17 +19,36 @@ public class App extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("Start.fxml"));
-
-        Scene scene = new Scene(root);
-
-        App.data.add("stage", primaryStage);
-        App.data.add("scene", scene);
+        Scene scene = new Scene(FXMLLoader.load(getClass().getResource("view/Start.fxml")));
 
         primaryStage.setTitle("Wordle");
-        primaryStage.setScene((Scene) App.data.get("scene"));
+        primaryStage.setScene(scene);
 
         primaryStage.show();
+
+        data.add("stage", primaryStage);
+        data.add("scene", scene);
+        data.addObserver("scene", (newScene) -> {
+            Stage stage = (Stage) data.get("stage");
+            stage.setScene((Scene) newScene);
+        });
+    }
+
+    public static void switchScenes(String fxmlPath) {
+        Scene scene;
+        try {
+            scene = new Scene(FXMLLoader.load(App.class.getResource("view/" + fxmlPath)));
+        } catch (IOException e) {
+            System.err.println("ERROR: Cannot read file");
+            e.printStackTrace();
+            return;
+        }
+
+        switchScenes(scene);
+    }
+
+    public static void switchScenes(Scene scene) {
+        data.set("scene", scene);
     }
 
 }
