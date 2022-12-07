a5e923f1356001ec34f599a56abf35be19569f42
Brayden Chan <72172597+megabyte6@users.noreply.github.com>
2022-12-06 23:16:17 -0800

    Adjust for new packages.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,7 +3,8 @@ module wordle {
     requires transitive javafx.controls;
     requires transitive javafx.graphics;
 
-    opens com.megabyte6.wordle to javafx.fxml;
+    opens com.megabyte6.wordle.controller to javafx.fxml;
 
     exports com.megabyte6.wordle;
+    exports com.megabyte6.wordle.util;
 }
