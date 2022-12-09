efe10a3ff02ac61f1840070d609b2128492c85fc
Brayden Chan <72172597+megabyte6@users.noreply.github.com>
2022-12-08 16:15:40 -0800

    Create game board.

--- src/main/java/com/megabyte6/wordle/controller/GameController.java
+++ src/main/java/com/megabyte6/wordle/controller/GameController.java
@@ -1,5 +1,15 @@
 package com.megabyte6.wordle.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+
 public class GameController {
+
+    @FXML
+    VBox root;
+
+    @FXML
+    GridPane gameBoard;
 
 }
