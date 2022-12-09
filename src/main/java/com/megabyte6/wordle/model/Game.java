eedc1be125ce3acf1b42f908765ab2968b7a9432
Brayden Chan <72172597+megabyte6@users.noreply.github.com>
2022-12-08 20:20:49 -0800

    Detect key presses.

--- src/main/java/com/megabyte6/wordle/model/Game.java
+++ src/main/java/com/megabyte6/wordle/model/Game.java
@@ -1,4 +1,29 @@
 package com.megabyte6.wordle.model;
 
+import com.megabyte6.wordle.App;
+
+import javafx.scene.input.KeyCode;
+import javafx.util.Duration;
+
 public class Game {
+
+    private char[][] gameBoard = new char[6][5];
+    private int numberOfAttempts = 0;
+
+    public Game() {
+        App.data.add("game", this);
+        App.switchScenes("Game.fxml", Duration.millis(500));
+    }
+
+    public void typeKey(KeyCode key) {
+        if (key == KeyCode.BACK_SPACE)
+            backspace();
+    }
+
+    public void backspace() {
+    }
+
+    public void checkWord() {
+    }
+
 }
