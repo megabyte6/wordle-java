221ca842ba6dbb252e54da2818729bb7652eac3c
Brayden Chan <72172597+megabyte6@users.noreply.github.com>
2022-12-08 20:20:35 -0800

    Detect key presses.

--- src/main/java/com/megabyte6/wordle/controller/GameController.java
+++ src/main/java/com/megabyte6/wordle/controller/GameController.java
@@ -1,15 +1,46 @@
 package com.megabyte6.wordle.controller;
 
+import com.megabyte6.wordle.App;
+import com.megabyte6.wordle.model.Game;
+
 import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.Label;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 
 public class GameController {
 
-    @FXML
-    VBox root;
+    private Game game = (Game) App.data.get("game");
 
     @FXML
-    GridPane gameBoard;
+    private VBox root;
+    @FXML
+    private GridPane gameBoard;
+
+    @FXML
+    private void keyPressed(KeyEvent event) {
+        switch (event.getCode()) {
+            case ENTER:
+                game.checkWord();
+                break;
+            default:
+                game.typeKey(event.getCode());
+                break;
+        }
+    }
+
+    public void setBox(String text, int row, int column) {
+        Label label = (Label) getNodeByPosition(row, column);
+        label.setText(text);
+    }
+
+    private Node getNodeByPosition(int row, int column) {
+        return gameBoard.getChildren().stream()
+                .filter(node -> GridPane.getRowIndex(node) == row && GridPane.getColumnIndex(node) == column)
+                .findFirst()
+                .get();
+    }
 
 }
