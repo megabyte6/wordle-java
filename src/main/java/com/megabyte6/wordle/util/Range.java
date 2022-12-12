7adfbb0d4db7aaad04c1387b128d1729152fe95b
Brayden Chan <72172597+megabyte6@users.noreply.github.com>
2022-12-12 14:38:45 -0800

    Create Range util class.

---
+++ src/main/java/com/megabyte6/wordle/util/Range.java
@@ -0,0 +1,43 @@
+package com.megabyte6.wordle.util;
+
+public class Range {
+
+    /**
+     * Creates an {@code int[]} of all integers from 0 to {@code to}. One
+     * common use case is in for-each loops to offer nicer syntactical sugar
+     * that many other languages provide.
+     * 
+     * @param to   Ending value (exclusive).
+     * @return     An {@code int[]} of integers to loop through.
+     */
+    public static int[] range(int to) {
+        int[] range = new int[to];
+        for (int i = 0; i < range.length; i++) {
+            range[i] = i;
+        }
+
+        return range;
+    }
+
+    /**
+     * Creates an {@code int[]} of all integers from {@code from} to
+     * {@code to}. One common use case is in for-each loops to offer nicer
+     * syntactical sugar that many other languages provide.
+     * 
+     * @param from Starting value (inclusive).
+     * @param to   Ending value (exclusive).
+     * @return     An {@code int[]} of integers to loop through.
+     */
+    public static int[] range(int from, int to) {
+        if (from >= to)
+            return new int[] {};
+
+        int[] range = new int[to - from];
+        for (int i = 0; i < range.length; i++) {
+            range[i] = from + i;
+        }
+
+        return range;
+    }
+
+}
