520cd383751117b3c8f687de02c8a56c31497af3
Brayden Chan <72172597+megabyte6@users.noreply.github.com>
2022-12-13 12:54:42 -0800

    Remove useless keylistener.

--- src/main/java/com/megabyte6/wordle/controller/StartController.java
+++ src/main/java/com/megabyte6/wordle/controller/StartController.java
@@ -3,20 +3,8 @@ package com.megabyte6.wordle.controller;
 import com.megabyte6.wordle.model.Game;
 
 import javafx.fxml.FXML;
-import javafx.scene.input.KeyEvent;
 
 public class StartController {
-
-    @FXML
-    private void keyPressed(KeyEvent event) {
-        switch (event.getCode()) {
-            case ENTER:
-                start();
-                break;
-            default:
-                break;
-        }
-    }
 
     @FXML
     private void start() {
