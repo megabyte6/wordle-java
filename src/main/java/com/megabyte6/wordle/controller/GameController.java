a8d12e214f3a7361a042dea9525d55a9e9cd6b01
Brayden Chan <72172597+megabyte6@users.noreply.github.com>
2022-12-17 00:58:53 -0800

    Fix scenebuilder bug.

--- src/main/java/com/megabyte6/wordle/controller/GameController.java
+++ src/main/java/com/megabyte6/wordle/controller/GameController.java
@@ -7,11 +7,10 @@ import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
 
-public class GameController {
+public class GameController implements Controller {
 
     private Game game = new Game(this);
 
@@ -20,9 +19,9 @@ public class GameController {
     @FXML
     private GridPane gameBoard;
 
-    @FXML
-    private void initialize() {
-        SceneManager.getScene().setOnKeyPressed((KeyEvent event) -> typeKey(event.getCode()));
+    @Override
+    public void initListeners() {
+        SceneManager.getScene().setOnKeyPressed(event -> typeKey(event.getCode()));
     }
 
     private void typeKey(KeyCode key) {
@@ -37,25 +36,34 @@ public class GameController {
         }
         if (game.cursorIsAtMaxIndex())
             return;
+
+        game.setLetter(key.getChar());
         game.incrementCursorIndex();
     }
 
     private void backspace() {
         if (game.cursorIsAtMinIndex())
             return;
+        game.decrementCursorIndex();
+        game.setLetter("");
     }
 
     private void winGame() {
     }
 
-    private void setBoxText(String text, int row, int column) {
+    public void setBoxText(String text, int row, int column) {
         Label label = (Label) getNodeByPosition(row, column);
         label.setText(text);
     }
 
     private Node getNodeByPosition(int row, int column) {
+        // Check if row or column is 0 because GridPane.getRowIndex() and
+        // GridPane.getColumnIndex() returns null for any indexes of 0.
+        Integer rowValue = row == 0 ? null : row;
+        Integer columnValue = column == 0 ? null : column;
         return gameBoard.getChildren().stream()
-                .filter(node -> GridPane.getRowIndex(node) == row && GridPane.getColumnIndex(node) == column)
+                .filter(node -> GridPane.getRowIndex(node) == rowValue
+                        && GridPane.getColumnIndex(node) == columnValue)
                 .findFirst()
                 .get();
     }
