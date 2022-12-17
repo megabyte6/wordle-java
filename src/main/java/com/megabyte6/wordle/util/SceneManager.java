0809d201797e821b26455300df6d69ad7fd4fb1e
Brayden Chan <72172597+megabyte6@users.noreply.github.com>
2022-12-17 00:58:21 -0800

    Reset scene instead of creating a newScene variable.

--- src/main/java/com/megabyte6/wordle/util/SceneManager.java
+++ src/main/java/com/megabyte6/wordle/util/SceneManager.java
@@ -2,11 +2,12 @@ package com.megabyte6.wordle.util;
 
 import java.io.IOException;
 
+import com.megabyte6.wordle.controller.Controller;
+
 import javafx.animation.FadeTransition;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
@@ -34,32 +35,33 @@ public class SceneManager {
     public static void switchScenes(String fxmlFileName, Duration totalTransitionDuration) {
         // Get old scene.
         Scene oldScene = SceneManager.scene;
-        Parent oldRoot = oldScene.getRoot();
         oldScene.setFill(SceneManager.backgroundColor);
 
         // Load new scene.
         final String path = "/com/megabyte6/wordle/view/" + fxmlFileName;
-        Scene newScene;
         try {
-            newScene = new Scene(FXMLLoader.load(SceneManager.class.getResource(path)));
+            FXMLLoader fxmlLoader = new FXMLLoader(SceneManager.class.getResource(path));
+            // Set the current scene to the new scene.
+            SceneManager.scene = new Scene(fxmlLoader.load());
+            SceneManager.scene.setFill(SceneManager.backgroundColor);
+            SceneManager.scene.getRoot().setOpacity(0);
+            // Initialize listeners.
+            Controller controller = fxmlLoader.getController();
+            controller.initListeners();
         } catch (IOException e) {
             System.err.println("ERROR: Cannot read file '" + path + "'");
             e.printStackTrace();
             return;
         }
-        newScene.setFill(SceneManager.backgroundColor);
-        newScene.getRoot().setOpacity(0);
-        Parent newRoot = newScene.getRoot();
-
-        // Set the current scene to the new scene.
-        SceneManager.scene = newScene;
 
         // Set up fade transitions.
-        FadeTransition oldSceneFadeOut = new FadeTransition(totalTransitionDuration.divide(2), oldRoot);
+        FadeTransition oldSceneFadeOut = new FadeTransition(
+                totalTransitionDuration.divide(2), oldScene.getRoot());
         oldSceneFadeOut.setFromValue(1);
         oldSceneFadeOut.setToValue(0);
 
-        FadeTransition newSceneFadeIn = new FadeTransition(totalTransitionDuration.divide(2), newRoot);
+        FadeTransition newSceneFadeIn = new FadeTransition(
+                totalTransitionDuration.divide(2), SceneManager.scene.getRoot());
         newSceneFadeIn.setFromValue(0);
         newSceneFadeIn.setToValue(1);
 
@@ -67,7 +69,7 @@ public class SceneManager {
         oldSceneFadeOut.setOnFinished(new EventHandler<ActionEvent>() {
             public void handle(ActionEvent event) {
                 // Swap scenes between transitions.
-                SceneManager.stage.setScene(newScene);
+                SceneManager.stage.setScene(SceneManager.scene);
                 // Start the new scene's fade-in animation.
                 newSceneFadeIn.play();
             };
