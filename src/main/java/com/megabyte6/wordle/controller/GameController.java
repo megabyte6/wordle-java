0ed2076a31bf09607c6332d93f34aa322753b38e
Brayden Chan <72172597+megabyte6@users.noreply.github.com>
2022-12-18 16:10:27 -0800

    Add animation when the user inputs an invalid word.

--- src/main/java/com/megabyte6/wordle/controller/GameController.java
+++ src/main/java/com/megabyte6/wordle/controller/GameController.java
@@ -1,14 +1,24 @@
 package com.megabyte6.wordle.controller;
 
+import static javafx.util.Duration.millis;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.fxexperience.javafx.animation.ShakeTransition;
 import com.megabyte6.wordle.model.Game;
 import com.megabyte6.wordle.util.SceneManager;
 
+import javafx.animation.FadeTransition;
+import javafx.animation.PauseTransition;
+import javafx.animation.SequentialTransition;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.input.KeyCode;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
+import javafx.util.Duration;
 
 public class GameController implements Controller {
 
@@ -19,26 +29,40 @@ public class GameController implements Controller {
     @FXML
     private GridPane gameBoard;
 
+    @FXML
+    private Label popup;
+
     @Override
     public void initListeners() {
         SceneManager.getScene().setOnKeyPressed(event -> typeKey(event.getCode()));
     }
 
     private void typeKey(KeyCode key) {
-        if (key == KeyCode.BACK_SPACE) {
-            backspace();
-            return;
-        }
-        if (key == KeyCode.ENTER) {
-            if (game.checkWord())
-                winGame();
-            return;
-        }
-        if (game.cursorIsAtMaxIndex())
-            return;
+        switch (key) {
+            case BACK_SPACE:
+                backspace();
+                break;
 
-        game.setLetter(key.getChar());
-        game.incrementCursorIndex();
+            case ENTER:
+                if (!game.cursorIsAtMaxIndex())
+                    break;
+                if (!game.isWord(game.getCurrentGuess())) {
+                    inputNotValid();
+                    break;
+                }
+                if (game.checkGuess())
+                    winGame();
+                break;
+
+            default:
+                if (game.cursorIsAtMaxIndex())
+                    break;
+                if (!key.isLetterKey())
+                    break;
+
+                game.setLetter(key.getChar());
+                game.incrementCursorIndex();
+        }
     }
 
     private void backspace() {
@@ -48,7 +72,44 @@ public class GameController implements Controller {
         game.setLetter("");
     }
 
+    private void inputNotValid() {
+        getNodesByRow(game.getAttemptNum()).stream()
+                .forEach(node -> {
+                    ShakeTransition shake = new ShakeTransition(node, millis(750), millis(0));
+                    shake.play();
+                });
+        popup("Not in word list");
+    }
+
     private void winGame() {
+    }
+
+    private void popup(String text) {
+        popup(text, millis(750), millis(200));
+    }
+
+    private void popup(String text, Duration pauseDuration, Duration transitionDuration) {
+        popup.setText(text);
+
+        final FadeTransition fadeIn = new FadeTransition();
+        fadeIn.setNode(popup);
+        fadeIn.setDuration(transitionDuration);
+        fadeIn.setFromValue(0);
+        fadeIn.setToValue(1);
+
+        final PauseTransition pause = new PauseTransition();
+        pause.setDuration(pauseDuration);
+
+        final FadeTransition fadeOut = new FadeTransition();
+        fadeOut.setNode(popup);
+        fadeOut.setDuration(transitionDuration);
+        fadeOut.setFromValue(1);
+        fadeOut.setToValue(0);
+
+        final SequentialTransition transition = new SequentialTransition();
+        transition.getChildren().addAll(fadeIn, pause, fadeOut);
+
+        transition.play();
     }
 
     public void setBoxText(String text, int row, int column) {
@@ -59,13 +120,28 @@ public class GameController implements Controller {
     private Node getNodeByPosition(int row, int column) {
         // Check if row or column is 0 because GridPane.getRowIndex() and
         // GridPane.getColumnIndex() returns null for any indexes of 0.
-        Integer rowValue = row == 0 ? null : row;
-        Integer columnValue = column == 0 ? null : column;
+        Integer rowValue = row == 0
+                ? null
+                : row;
+        Integer columnValue = column == 0
+                ? null
+                : column;
         return gameBoard.getChildren().stream()
                 .filter(node -> GridPane.getRowIndex(node) == rowValue
                         && GridPane.getColumnIndex(node) == columnValue)
                 .findFirst()
                 .get();
+    }
+
+    private List<Node> getNodesByRow(int row) {
+        // Check if row or column is 0 because GridPane.getRowIndex() and
+        // GridPane.getColumnIndex() returns null for any indexes of 0.
+        Integer rowValue = row == 0
+                ? null
+                : row;
+        return gameBoard.getChildren().stream()
+                .filter(node -> GridPane.getRowIndex(node) == rowValue)
+                .collect(Collectors.toList());
     }
 
 }
