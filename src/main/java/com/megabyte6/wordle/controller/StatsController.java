8188af200dfb858bc25d9fce06926a309ce1bf88
Brayden Chan <braydenmchan@gmail.com>
2023-01-01 14:56:44 -0800

    Show basic stats.

--- src/main/java/com/megabyte6/wordle/controller/StatsController.java
+++ src/main/java/com/megabyte6/wordle/controller/StatsController.java
@@ -1,29 +1,37 @@
 package com.megabyte6.wordle.controller;
 
-import com.megabyte6.wordle.model.Game;
+import com.megabyte6.wordle.App;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 
 public class StatsController extends Controller {
-
-    private Game game;
 
     private Runnable runOnClose = () -> {
     };
 
+    @FXML
+    private Label gamesPlayed;
+    @FXML
+    private Label winPercentage;
+    @FXML
+    private Label currentStreak;
+    @FXML
+    private Label longestStreak;
+
     @Override
     public void initialize() {
-        if (game == null)
+        if (App.stats == null)
             return;
+        gamesPlayed.setText(App.stats.numberOfGamesPlayed() + "");
+        winPercentage.setText(App.stats.numberOfWins() / App.stats.numberOfGamesPlayed() * 100 + "%");
+        currentStreak.setText(App.stats.currentWinStreak() + "");
+        longestStreak.setText(App.stats.longestWinStreak() + "");
     }
 
     @FXML
     private void close() {
         runOnClose.run();
-    }
-
-    public void setGame(Game game) {
-        this.game = game;
     }
 
     public void runOnClose(Runnable runOnClose) {
