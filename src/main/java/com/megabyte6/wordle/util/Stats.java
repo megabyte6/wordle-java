7fe834aeec2ae72864de2f4369f92315490b6a50
Brayden Chan <braydenmchan@gmail.com>
2023-01-01 21:17:39 -0800

    Calculate winPercentage.

--- src/main/java/com/megabyte6/wordle/util/Stats.java
+++ src/main/java/com/megabyte6/wordle/util/Stats.java
@@ -1,5 +1,12 @@
 package com.megabyte6.wordle.util;
 
+import java.io.Serializable;
+
 public record Stats(int numberOfGamesPlayed, int numberOfWins, int currentWinStreak, int longestWinStreak,
-        int[] guessDistribution) {
+        int[] guessDistribution) implements Serializable {
+
+    public int winPercentage() {
+        return (int) (100.0 * numberOfWins / numberOfGamesPlayed);
+    }
+
 }
