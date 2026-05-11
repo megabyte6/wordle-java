bdc4c7c8877e5ff9250f71f16c1247315260888b
Brayden Chan <72172597+megabyte6@users.noreply.github.com>
2026-05-11 05:58:12 -0700

    Switch to kotlin dsl for gradle build scripts

---
+++ build.gradle.kts
@@ -0,0 +1,73 @@
+plugins {
+    id("java")
+    id("application")
+    id("org.openjfx.javafxplugin") version "0.0.13"
+    id("org.beryx.jlink") version "2.25.0"
+}
+
+val appVersion: String by project
+val javafxVersion: String by project
+val jpackageFormat: String by project
+
+version = appVersion
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
+}
+
+application {
+    mainClass.set("com.megabyte6.wordle.App")
+    mainModule.set("wordle")
+}
+
+javafx {
+    version = javafxVersion
+    modules = listOf("javafx.controls", "javafx.fxml")
+}
+
+jlink {
+    options.set(listOf(
+        "--strip-debug",
+        "--compress", "2",
+        "--no-header-files",
+        "--no-man-pages",
+    ))
+    launcher {
+        noConsole = true
+    }
+
+    imageZip.set(layout.buildDirectory.file("wordle.zip"))
+
+    jpackage {
+        imageName = "Wordle"
+        installerName = "wordle-installer"
+        icon = "src/main/resources/icon.ico"
+        vendor = "Brayden Chan"
+
+        installerOptions = if (jpackageFormat != "default") {
+            listOf("--type", jpackageFormat)
+        } else {
+            emptyList()
+        }
+
+        if ("Windows" in System.getProperty("os.name")) {
+            installerOptions.addAll(listOf(
+                "--win-dir-chooser",
+                "--win-menu",
+                "--win-menu-group", "Wordle",
+                "--win-per-user-install",
+                "--win-shortcut",
+                "--win-shortcut-prompt",
+            ))
+        }
+    }
+}
